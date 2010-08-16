585a72461ead521a4796a9abaec286986001177c
SND\LarsCorneliussen_cp <SND\LarsCorneliussen_cp@91bfc952-fec6-4be4-b192-9eb3b8389945>
2010-08-16 19:33:53 +0000

    Tag for 0.2
    git-svn-id: https://appinfo.svn.codeplex.com/svn/tags/appinfo-0.2@1354 91bfc952-fec6-4be4-b192-9eb3b8389945

---
+++ AppInfo/AssemblyExtensions.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace AppInfo
+{
+    public static class AssemblyExtensions
+    {
+        public static ApplicationInformation GetInfo(this Assembly assembly)
+        {
+            var attributes = assembly.GetCustomAttributes(false);
+
+            var info = new ApplicationInformation();
+
+            info.Title = PickOrNull<AssemblyTitleAttribute>(
+                attributes, a => a.Title);
+
+            info.Description = PickOrNull<AssemblyDescriptionAttribute>(
+                attributes, a => a.Description);
+
+            info.Copyright = PickOrNull<AssemblyCopyrightAttribute>(
+                attributes, a => a.Copyright);
+
+            Version assemblyVersion = assembly.GetName().Version;
+            info.VersionString = assemblyVersion.Major + "." + assemblyVersion.Minor;
+
+            return info;
+        }
+
+        private static string PickOrNull<T>(IEnumerable<object> attributes, Func<T, string> func)
+            where T : Attribute
+        {
+            var attr = attributes.OfType<T>()
+                .SingleOrDefault();
+
+            if (attr != null)
+                return func(attr);
+
+            return null;
+        }
+    }
+}
